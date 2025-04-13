64f4cd36e4b438c9e90afe0d88120d669c095984
Marian <marian.lippold@student.hs-rm.de>
2025-04-13 15:55:03 +0200

    draw player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     # print("Starting Asteroids!")
@@ -10,13 +11,21 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2) # is a circle with default radius PLAYER_RADIUS
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(60)/1000 #returns DeltaTime; runs every 1/60 of a second
 
 
 if __name__ == "__main__":
---
+++ player.py
@@ -0,0 +1,21 @@
+from pygame import Vector2, draw
+import circleshape
+from constants import PLAYER_RADIUS
+
+class Player(circleshape.CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+
+        self.rotation = 0
+
+    # in the player class
+    def triangle(self):
+        forward = Vector2(0, 1).rotate(self.rotation)
+        right = Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+
+    def draw(self, screen):
+        draw.polygon(screen, "white", self.triangle(), 2)
